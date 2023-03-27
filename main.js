2b7806da6881744ccc8d716d0d8f0c1aca966de6
Angel Romero <mrangelrcd@gmail.com>
2023-03-28 01:00:57 +0200

    Add books to library functional.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
 /* eslint-disable no-use-before-define */
 const myLibrary = [];
 const openModalButton = document.querySelector(".add-book-button");
 const addBookModal = document.querySelector("dialog");
-const addBookButton = document.querySelector("dialog button");
+const form = document.querySelector(".submit-new-book");
 
 function Book(title, author, pages, readen) {
   this.title = title;
@@ -17,28 +19,34 @@ function Book(title, author, pages, readen) {
   };
 }
 
+Book.prototype.toggleRead = function () {
+  this.readen = !this.readen;
+};
+
 function addBookToLibrary(bookData) {
   const book = new Book(...bookData);
   myLibrary.push(book);
 }
 
-addBookButton.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const form = document.querySelector(".submit-new-book");
 
   const formData = new FormData(form);
   const newBook = Array.from(formData.values());
   if (newBook.length === 3) {
     newBook.push(false);
+  } else {
+    newBook[3] = true;
   }
 
-  console.log(newBook);
+  addBookToLibrary(newBook);
 
   // Empty form and close modal after adding book
-  formData.forEach((value, key) => {
-    formData.set(key, "");
-  });
+  const inputs = form.querySelectorAll("input");
+  inputs.forEach((input) => (input.value = ""));
   addBookModal.close();
+  // TODO delete
+  console.table(myLibrary);
 });
 
 openModalButton.addEventListener("click", () => {
